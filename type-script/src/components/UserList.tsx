3b88107abc40163fd1af7d4c9c36d87794ba0dcc
BeZJIuKiy <matveenkov.sasha@gmail.com>
2021-05-12 12:02:13 +0300

    upload users

---
+++ type-script/src/components/UserList.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useTypeSelector } from '../hooks/useTypeSelector';
+import { fetchUsers } from '../store/action-creator/user';
+
+// Примечание: React.FC - обозначение функционального компонента
+export const UserList: React.FC = () => {
+
+    // Получаем текущее состояние
+    const { users, loading, error } = useTypeSelector(state => state.user);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchUsers());
+    }, []);
+
+    if (loading) {
+        return <h1>Идет загрузка...</h1>
+    }
+    if (error) {
+        return <h1>{error}</h1>
+    }
+
+    return (
+        <div>
+            {users.map(user => 
+                <div key={user.id}>{user.name}</div>
+            )}
+        </div>
+    );
+};
--- type-script/src/store/reducers/index.ts
+++ type-script/src/store/reducers/index.ts
@@ -4,4 +4,8 @@ import { userReducer } from './userReducer';
 
 export const rootReducer = combineReducers({
     user: userReducer,
-})
+    todo: userReducer,
+});
+
+// Создаем свой хук, получаем необходимый тип: 
+export type RootState = ReturnType<typeof rootReducer>;
